f0dad6513f2e7e1410c0cf8a1dc0a0057cd4ec76
junichi <tmdjnch0901@gmail.com>
2022-09-22 23:10:51 +0900

    first commit

---
+++ front/pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div v-if="user">
+    <p>名前：{{user.name}}</p>
+    <AddTodo @submit="addTodo" />
+    <TodoList :todos="user.todos" />
+  </div>
+</template>
+
+<script>
+import AddTodo from "@/components/AddTodo";
+import TodoList from "@/components/TodoList";
+import axios from "@/plugins/axios";
+
+export default {
+  components: {
+    AddTodo,
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  // ここから追加
+  computed: {
+    user() {
+      return this.$store.state.auth.currentUser;
+    }
+  },
+  methods: {
+    async addTodo(todo) {
+      const { data } = await axios.post("/v1/todos", { todo });
+      this.$store.dispatch("auth/setUser", {
+        ...this.user,
+        todos: [...this.user.todos, data]
+      });
+    },
+    fetch({
+      store,
+      redirect
+    }) {
+      store.watch(
+        state => state.auth.currentUser,
+        (newUser, oldUser) => {
+          if (!newUser) {
+            return redirect("/login");
+          }
+        }
+      );
+    },
+  },
+};
+
+</script>
+
+<style>
+
+</style>
+ß
